9f563abc40d5f09724d193f729d5fb760fa534bf
gustavo <gustavo@tresastronautas.com>
2024-12-29 01:23:30 -0500

    feat: Inclusión de ruteo basico.

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export * from "./Footer";
+export * from "./Header";
+export * from "./Home";
+export * from "./Users";
+export * from "./Register";
+export * from "./Login";
+export * from "./SideNav";
